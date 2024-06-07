90fd99ad4111d128d6e570c5377d99602120400a
Dot <hey@dotty.cool>
2024-06-07 23:23:19 +0000

    lookin pretty good

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.png';
+
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="hero">
+        <div className="title">
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 450 100"
+            preserveAspectRatio="xMinYMid meet"
+          >
+            {"BIGTEAM".split('').map((letter, i) => (
+              <text
+                key={i}
+                x={450.0 / 7 * (i + 0.5)}
+                y="80"
+                fontSize="75"
+                fill="currentColor"
+                textAnchor="middle"
+              >
+                {letter}
+              </text>
+            ))}
+          </svg>
+        </div>
+        <div className="zilla">
+          <img className="z" src="/monsters/test.webp " />
+          <p className="zillaAttribution">Godzilla - Showa Era</p>
+        </div>
+        <div className="skyline" />
       </header>
+      <nav>
+        <button type="button">
+          home
+        </button>
+        <button type="button">
+          about
+        </button>
+        <button type="button">
+          learn more
+        </button>
+      </nav>
+      <main>
+        <h1>Welcome to big team</h1>
+      </main>
+      {/* <HorizontalBannerRuling /> */}
+      <footer>
+        <p>follow us on twitter</p>
+      </footer>
+    </div>
+  );
+}
+
+const HorizontalBannerRuling = (props) => {
+  const { title, bannerStyle } = props;
+
+  return (
+    <div className="strips">
+      <div className="strip" style={{ backgroundColor: 'brown' }}>
+
+      </div>
+      <div className="strip" style={{ backgroundColor: 'green' }}>
+        {title}
+      </div>
+      <div className="strip" style={{ backgroundColor: 'skyblue' }}>
+        
+      </div>
     </div>
   );
 }
